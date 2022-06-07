dae927246946c8d13c69ec4233035386e80997e7
giuseppemaresca <marescag146@gmail.com>
2022-06-07 16:55:13 +0200

    EsercizioCorretto

---
+++ src/jana60/CalcolaBiglietto.java
@@ -0,0 +1,41 @@
+package jana60;
+import java.util.Scanner;
+public class CalcolaBiglietto {
+
+	public static void main(String[] args) {
+		double km;
+		int eta;
+		double prezzoPerKm=0.21;
+		double percentualeScontoMinorenni=0.20;
+		double percentualeScontoOverSessantacinque=0.40;
+		Scanner scan = new Scanner (System.in);
+		System.out.print ("Inserisci Km da percorrere");
+		km = scan.nextDouble();
+		System.out.print("Inserisci eta");
+		eta=scan.nextInt();
+		double prezzoTotale=km*prezzoPerKm;
+		//Prezzo per i minorenni 
+		double scontoMinorenni=prezzoTotale*percentualeScontoMinorenni;
+		
+		
+	//Prezzo età maggiore di 65
+		double scontoOverSessantacinque=prezzoTotale*percentualeScontoOverSessantacinque;
+		
+		if (eta<18)
+		{
+			double prezzoFinale=prezzoTotale-scontoMinorenni;
+			System.out.print(prezzoFinale);
+		}else if(eta<=18 && eta>=65)
+		{ 
+			double prezzoFinale=prezzoTotale;
+			System.out.print(prezzoFinale);
+			
+		}else {
+		  double prezzoFinale=prezzoTotale-scontoOverSessantacinque;
+		  System.out.print(prezzoFinale);
+		}
+		
+		 scan.close();
+	}
+
+}
